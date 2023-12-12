eb9621bce022538ce9ebb4c07f875f48191e9876
yuuki1293 <yuuki1293@outlook.jp>
2023-12-12 13:38:06 +0900

    SMP10_4

---
+++ SMP10_4/SMP10_4.asm
@@ -0,0 +1,98 @@
+    LIST P=PIC16F819
+    #INCLUDE <P16F819.INC>
+
+    __CONFIG    _HS_OSC & _CP_OFF & _WDT_OFF & _PWRTE_ON & _MCLR_ON & _LVP_OFF
+
+;uart
+CNT1        EQU 0EH
+CNTB        EQU 0FH
+IN          EQU 012H
+ADRES       EQU 013H
+
+;ad
+CNT         EQU 020H
+CNTDEG      EQU 021H
+DIVIDEND    EQU 022H
+BIT         EQU 023H
+
+    ORG     0H
+
+MAIN
+    BSF     STATUS, RP0
+    MOVLW   01H
+    MOVWF   TRISA
+    CLRF    TRISB
+    MOVLW   b'10001110'
+    MOVWF   ADCON1
+    BCF     STATUS, RP0
+    MOVLW   081H
+    MOVWF   ADCON0
+    CLRF    PORTA
+    CLRF    PORTB
+
+ADSTART
+    CALL    TIME20U
+    BSF     ADCON0, GO
+ADLOOP
+    BTFSC   ADCON0, GO
+    GOTO    ADLOOP
+
+;Tx begin
+;b00010000送信
+    MOVLW   b'00010000'
+    CALL    CSEND
+;9:8ビット送信
+    MOVF    ADRESH, W
+    CALL    CSEND
+;7:4ビット送信
+    MOVF    ADRESL, W
+    MOVWF   ADRES
+    SWAPF   ADRES, W
+    ANDLW   b'00001111'
+    CALL    CSEND
+;3:0ビット送信
+    MOVF    ADRESL, W
+    ANDLW   b'00001111'
+    CALL    CSEND
+
+    GOTO    ADSTART
+
+TIME20U
+    MOVLW   020H
+    MOVWF   CNT
+    NOP
+LOOP
+    DECFSZ  CNT, F
+    GOTO    LOOP
+    RETURN
+
+CSEND
+    MOVWF   IN
+    MOVLW   08H
+    MOVWF   CNTB
+    BCF     PORTA, 4
+    CALL    TIME104
+
+CSENDLP
+    BTFSC   IN, 0
+    BSF     PORTA, 4
+    BTFSS   IN, 0
+    BCF     PORTA, 4
+    CALL    TIME104
+    RRF     IN, F
+    DECFSZ  CNTB, F
+    GOTO    CSENDLP
+    BSF     PORTA, 4
+    CALL    TIME104
+    RETURN
+
+TIME104
+    MOVLW   082H
+    MOVWF   CNT1
+LOOP104
+    NOP
+    DECFSZ  CNT1, F
+    GOTO    LOOP104
+    RETURN
+
+    END 
